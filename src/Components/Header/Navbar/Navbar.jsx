38cf491d6105cb40a1a3bb5be016bdfa2f38bf47
ares-oykot <soykotislam131415@gmail.com>
2023-09-24 22:05:00 +0600

    added navbar section

---
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../../assets/Logo.png";
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center">
+      <div className="">
+        <img className="w-28 md:w-48" src={logo} alt="" />
+      </div>
+      <div className="">
+        <ul className="flex font-normal text-sm md:text-base gap-3 md:gap-5">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline font-bold"
+                  : ""
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/donation"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline"
+                  : ""
+              }
+            >
+              Donation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/statistics"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "pending"
+                  : isActive
+                  ? "text-[#FF444A] underline"
+                  : ""
+              }
+            >
+              Statistics
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Components/Header/Navbar/Navbar";
+
+const MainLayout = () => {
+    
+    return (
+        <div className="max-w-screen-xl mx-auto px-4 lg:px-0 mt-4">
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainLayout;
